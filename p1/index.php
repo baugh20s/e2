3ad9e34fc845cd0a73527a8056961f10ead2b805
Sarah Baughman <sbaughman11@gmail.com>
2019-09-17 21:06:14 -0400

    attempt to fix image

--- p1/index.php
+++ p1/index.php
@@ -10,7 +10,7 @@
 
     <h1>Sarah Baughman</h1>
 
-    <img src="img_3577.jpg" alt="Sarah's bio pic" height="1000" width="auto">
+    <img src="/img_3577.jpg" alt="Sarahs bio pic" height="1000" width="auto">
     
     <p>
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
