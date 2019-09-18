a875750bda83573337d82b70cbaddc19efdb1e07
Sarah Baughman <sbaughman11@gmail.com>
2019-09-17 21:37:16 -0400

    second centering attempt

--- p1/index.php
+++ p1/index.php
@@ -5,18 +5,22 @@
 	<title>p1</title>
 	<meta charset='utf-8'>
 
-    <link href="css/p1.css" rel="stylesheet">
+    <link href="/css/p1.css" rel="stylesheet">
 
 </head>
 <body>
 
     <h1>Sarah Baughman</h1>
 
+    <div class="container">
+
     <img src="/images/sarahBio.jpg" alt="Sarahs bio pic" height="100" width="auto">
     
     <p>
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
     </p>
+
+    </div>
 	
 </body>
 </html>
