ea334d0a9abd245a1a0f170bacb78fb746e81717
Sarah Baughman <sbaughman11@gmail.com>
2019-09-24 16:50:05 -0400

    updated p2 index

--- p2/index.php
+++ p2/index.php
@@ -1,10 +1,10 @@
 <!doctype html>
 <html lang='en'>
 <head>
-	<title>Practice Application</title>
+	<title>Project 2</title>
 	<meta charset='utf-8'>
 </head>
 <body>
-	<h1>Practice Application</h1>
+	<h1>Project 2</h1>
 </body>
 </html>
