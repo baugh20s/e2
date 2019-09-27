ded54589c460a87df823b96b295f99a3aca4d23c
Sarah Baughman <sbaughman11@gmail.com>
2019-09-26 20:41:46 -0400

    created array and testing with vardump

--- p2/index.php
+++ p2/index.php
@@ -5,6 +5,25 @@
 	<meta charset='utf-8'>
 </head>
 <body>
-	<h1>Project 2</h1>
+	<h1>Rock, Paper, Scissors!</h1>
+	<p>ADD INSTRUCTIONS</p>
+	
+	<?php
+		//player A & player B
+		//rock, paper, scissors
+
+		$moves = [
+			'rock',
+			'paper',
+			'scissors',
+		];
+
+		var_dump($moves[0]);
+		var_dump($moves[1]);
+		var_dump($moves[2]);
+
+
+	?>
+
 </body>
 </html>
